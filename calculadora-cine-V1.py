67a0f92ac20615610f6a084da5d61406dab41cb7
María Lacasa <66799716+lamilhouse@users.noreply.github.com>
2026-04-15 13:41:06 +0200

    Update calculadora-cine-V1.py

--- calculadora-cine-V1.py
+++ calculadora-cine-V1.py
@@ -184,13 +184,6 @@ st.write("")
 st.write("---") 
 
 with st.expander("Información, privacidad y contacto"):
-    st.markdown("""
-    ### ¿Para qué sirve esta app?
-    A menudo, al recibir la nómina, nos enfrentamos a dos retos:
-    - **Falta de claridad:** los conceptos, retenciones y bases suelen ser difíciles de descifrar a primera vista.
-    - **Incertidumbre:** no siempre tenemos la certeza de si lo ingresado coincide con las jornadas, horas extras y dietas realmente realizadas.
-
-    Esta calculadora te permite prever tu salario neto de forma rápida antes de recibir la liquidación oficial.
 
     ### Cómo tener la app en tu móvil
     Si quieres tener esta calculadora siempre a mano, puedes crear un **acceso directo** en tu pantalla de inicio:
@@ -199,13 +192,15 @@ with st.expander("Información, privacidad y contacto"):
     3. Selecciona la opción **"Añadir a pantalla de inicio"**.
     4. Si no te aparece en el inicio la encontrarás entre tus apps como "Streamlit"
 
-    ### Normativa y Prácticas del Sector
+    ### Normativa y prácticas del sector
     Los cálculos se realizan tomando como referencia la normativa vigente (convenio colectivo, normativa de la Seguridad Social, tablas de IRPF, conceptos salariales estándar...). No obstante, nos encontraremos algún apartado donde la lógica de la app se basa en las prácticas más habituales y extendidas de nuestra industria, lo cual puede hacer que no coincida exactamente tu nómina con el resultado mostrado.
 
-    ### Privacidad Total
+    ### Privacidad
     Esta aplicación **no almacena ningún dato introducido**. Ni salarios, ni horas, ni retenciones. Al cerrar la pestaña del navegador o pulsar el botón "Nuevo cálculo", todos los datos introducidos desaparecen para siempre.
 
-    **IMPORTANTE:** Los resultados ofrecidos por esta calculadora son meramente informativos y orientativos. En ningún caso tienen carácter vinculante ni valor legal oficial. Esta herramienta no sustituye el asesoramiento de un profesional gestor o la información proporcionada por los departamentos de RRHH. El creador de esta aplicación no se hace responsable de discrepancias entre los resultados de la app y las nóminas reales.
+    **IMPORTANTE** 
+    Los resultados ofrecidos por esta calculadora son meramente informativos y orientativos. En ningún caso tienen carácter vinculante ni valor legal oficial. Esta herramienta no sustituye el asesoramiento de un profesional gestor o la información proporcionada por los departamentos de RRHH. El creador de esta aplicación no se hace responsable de discrepancias entre los resultados de la app y las nóminas reales.
 
+        
     👉 [**Envíanos tus comentarios, dudas, sugerencias... haciendo clic aquí**](https://forms.gle/CWvr3USetYqbdam8A)
     """)
